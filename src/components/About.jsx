d1dc6542dfed37afc5e4ce1e6ccb89a663649598
Leewell-Muchiri <leewellmacharia10@gmail.com>
2023-05-11 17:21:50 +0300

    my-portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Tilt } from 'react-tilt';
+import { motion } from "framer-motion";
+
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I am a passionate web developer keen on joining like-minded developers in providing creative solutions. As an active
+        listener who takes great interest in technology, I am constantly eager to engage the world and collaborate with others to
+        use technology as a gateway towards innovative solutions for complex problems. I am constantly improving my skills and
+        challenging myself to offer exemplary solutions.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,165 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Frontend Engineer",
+      icon: web,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    // {
+    //   name: "Ruby",
+    //   icon: ruby,
+    // },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+   
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "figma",
+      icon: figma,
+    },
+   
+  ];
+  
+  const experiences = [
+    {
+      title: "Full Stack Developer",
+      company_name: "Code City",
+      date: "March 2023 - April 2023",
+      points: [
+        "Developing and maintaining web applications using React.js and Ruby on Rails.",
+        "Collaborating with a team to create high-quality educational platform",
+        "Implementing responsive dashboard for both the educators and the students.",
+        "Implementing a postgresql as the database for the project.",
+      ],
+    },
+    {
+      title: "Web Developer",
+      company_name: "My-Duka",
+      date: "Jan 2023 - Jan 2023",
+      points: [
+        "Developing a web app using React.js and sinatra.",
+        "Collaborating with a team to offer simple app achieving desired functionality.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but the Code-city team proved me wrong.",
+      name: "Leila Jean",
+      designation: "Chief Engineer",
+      company: "WellCent io",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Shuleni",
+      description:
+        "Web-based platform for schools.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
